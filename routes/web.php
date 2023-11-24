d87783170f8ffee050eef89546ab38055c242a49
Gpaqsa <giorgi.paksashvili.2@btu.edu.ge>
2023-11-24 23:59:57 +0400

    Start Laravel Project

--- routes/web.php
+++ routes/web.php
@@ -13,49 +13,49 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Route::get('/aboutMe', function(){
+//     $data = "Giorgi Pakssahvili is  22 years old, Hobby Basketball";
+//     return $data;
 // });
 
+// Route::get('/fullname', function(){
+//     $data = "Giorgi Paksashvili";
+//     return $data;
+// });
 
-Route::get('/aboutMe', function(){
-    $data = "Giorgi Pakssahvili is  22 years old, Hobby Basketball";
-    return $data;
-});
+// Route::get('/years', function(){
+//     $data = "22 years old";
+//     return $data;
+// });
 
-Route::get('/fullname', function(){
-    $data = "Giorgi Paksashvili";
-    return $data;
-});
+// Route::get('/hob', function(){
+//     $data = "Hob is basketball";
+//     return $data;
+// });
 
-Route::get('/years', function(){
-    $data = "22 years old";
-    return $data;
-});
+// Route::get('/result', function () {
+//     $data = 'Result is nothing';
+//     return $data;
+// });
 
-Route::get('/hob', function(){
-    $data = "Hob is basketball";
-    return $data;
-});
+// Route::get('/', function () {
+//     $data = 'Giorgi paksashvili 22 years old, HOb is Basketball';
+//     return $data;
+// });
 
-Route::get('/result', function () {
-    $data = 'Result is nothing';
-    return $data;
-});
+// Route::put('/update', function(){
+//     return response()->json(['message'=> "წარმატებით განახლდა"]);
+// });
 
-Route::get('/', function () {
-    $data = 'Giorgi paksashvili 22 years old, HOb is Basketball';
-    return $data;
-});
+// Route::post("/add", function(){
+//     return response()->json(['message'=> "წარმატებით დაემატა"]);
+// });
 
-Route::put('/update', function(){
-    return response()->json(['message'=> "წარმატებით განახლდა"]);
-});
-
-Route::post("/add", function(){
-    return response()->json(['message'=> "წარმატებით დაემატა"]);
-});
-
-Route::delete('/delete', function(){
-    return response()->json(['message'=> "წარმატებით წაიშალა"]);
-});
+// Route::delete('/delete', function(){
+//     return response()->json(['message'=> "წარმატებით წაიშალა"]);
+// });
